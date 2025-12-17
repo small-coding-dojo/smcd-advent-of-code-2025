432de390e6c693cd076b27db252a2dbfd407a00b
Michael Mahlberg <mm@consulting-guild.de>
2025-12-17 17:21:57 +0100

    We kind of can count the number of circuits we created
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Stefan Boos <sboos@igus.net>
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>

--- src/ts/playground.ts
+++ src/ts/playground.ts
@@ -3,7 +3,17 @@ export class Playground{
         return this.junctionBoxes.length;
     }
     junctionBoxes: { x: number; y: number; z: number; }[] = [];
+    theNumberOfCircuits : number = 0;
     addJunctionBox(junctionBox: { x: number; y: number; z: number; }){
         this.junctionBoxes.push(junctionBox);
+        ++this.theNumberOfCircuits;
+    }
+
+    numberOfCircuits() {
+        return this.theNumberOfCircuits;
+    }
+
+    connect(x: number) {
+        --this.theNumberOfCircuits;
     }
 }
--- test/ts/playground.test.ts
+++ test/ts/playground.test.ts
@@ -6,6 +6,7 @@ describe ('Playground', () => {
         var subject = new Playground();
 
         expect(subject.numberOfJunctionBoxes()).to.equal(0);
+        expect(subject.numberOfCircuits()).to.equal(0);
     })
 
     it ('can collect junction boxes', () => {
@@ -15,5 +16,19 @@ describe ('Playground', () => {
         subject.addJunctionBox({x: 2, y:2, z: 2});
 
         expect(subject.numberOfJunctionBoxes()).to.equal(2);
+        expect(subject.numberOfCircuits()).to.equal(2);
     })
+
+    it ('can create a circuit', () => {
+        var subject = new Playground();
+
+        subject.addJunctionBox({x: 1, y:1, z: 1});
+        subject.addJunctionBox({x: 2, y:2, z: 2});
+
+        subject.connect(1);
+
+        expect(subject.numberOfJunctionBoxes()).to.equal(2);
+        expect(subject.numberOfCircuits()).to.equal(1);
+    })
+
 });
